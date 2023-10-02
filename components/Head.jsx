a8161cbeec3d1e6f8c406fc46db35108d9511d59
sahkritikDAOstruct <kritiksahco@gmail.com>
2023-10-02 23:46:13 +0530

    Portfolio SEO v2

--- components/Head.jsx
+++ components/Head.jsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const CustomHead = ({ title }) => {
   return (
@@ -12,13 +12,26 @@ const CustomHead = ({ title }) => {
         name="keywords"
         content="kritik sah, kritik, sah, web developer portfolio, kritik web developer, kritik developer, mern stack, kritik sah portfolio, vscode-portfolio, frontend, reactjs developer, react developer, nextjs developer, indian web developer"
       />
-      <meta property="og:title" content="Kritik sah - Fullstack web dev Portfolio" />
+      <meta
+        property="og:title"
+        content="Kritik sah - Fullstack web dev Portfolio"
+      />
+      <meta
+        name="twitter:title"
+        content="Kritik sah - Fullstack web dev Portfolio"
+      />
       <meta
         property="og:description"
         content="A full-stack developer building websites that you'd like to use."
       />
+      <meta
+        property="twitter:description"
+        content="A full-stack developer building websites that you'd like to use."
+      />
       <meta property="og:image" content="/projects/portfolio.png" />
+      <meta property="twitter:image" content="/projects/portfolio.png" />
       <meta property="og:url" content="https://www.cryptolancer.in/" />
+      <meta property="twitter:url" content="https://www.cryptolancer.in/" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
@@ -27,5 +40,5 @@ const CustomHead = ({ title }) => {
 export default CustomHead;
 
 CustomHead.defaultProps = {
-  title: 'Kritik Sah',
+  title: "Kritik Sah",
 };
