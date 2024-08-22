b1bbf9eeb5ae7e115c2a3b76c0dc7fd64005f589
Kritik <kritiksah0@gmail.com>
2024-08-22 17:33:58 +0530

    update image link

--- pages/articles.jsx
+++ pages/articles.jsx
@@ -1,11 +1,11 @@
-import ArticleCard from '../components/ArticleCard';
-import styles from '../styles/ArticlesPage.module.css';
+import ArticleCard from "../components/ArticleCard";
+import styles from "../styles/ArticlesPage.module.css";
 
 const ArticlesPage = ({ articles }) => {
   return (
     <>
       <h3>
-        Recent Posts from{' '}
+        Recent Posts from{" "}
         <a
           href="https://dev.to/kritik"
           target="_blank"
@@ -16,9 +16,11 @@ const ArticlesPage = ({ articles }) => {
         </a>
       </h3>
       <div className={styles.container}>
-        {articles.map((article) => (
-          <ArticleCard key={article.id} article={article} />
-        ))}
+        {articles.length > 0
+          ? articles.map((article) => (
+              <ArticleCard key={article.id} article={article} />
+            ))
+          : "Loading..."}
       </div>
     </>
   );
@@ -26,10 +28,10 @@ const ArticlesPage = ({ articles }) => {
 
 export async function getStaticProps() {
   const res = await fetch(
-    'https://dev.to/api/articles/me/published?per_page=6',
+    "https://dev.to/api/articles/me/published?per_page=6",
     {
       headers: {
-        'api-key': process.env.DEV_TO_API_KEY,
+        "api-key": process.env.DEV_TO_API_KEY,
       },
     }
   );
@@ -37,7 +39,7 @@ export async function getStaticProps() {
   const data = await res.json();
 
   return {
-    props: { title: 'Articles', articles: data },
+    props: { title: "Articles", articles: data },
     revalidate: 60,
   };
 }
